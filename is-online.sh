886d3f3b9cf7cd8b75a9d74af97e1e74dfa0b339
stein5750 <stein.by@outlook.com>
2022-05-23 14:28:57 +0200

    Added comment

--- is-online.sh
+++ is-online.sh
@@ -4,8 +4,8 @@
 # @param: url
 # @return: true|false True if there is an response. Else false.
 
+# Variables
 URL=$1
-
 REGEX='^(https?://|www\.|localhost)[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*$'
 
 # A simple check to see if the argument is an url.
